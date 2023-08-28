bea18f9da95656a1f9422c0cd8904df2150ea7ef
Alain Noel <alain.noel34@gmail.com>
2023-08-28 12:48:06 +0200

    start components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+
+  return (
+      <footer>
+          <div className="footer-container">
+              <p>Projet réalisé par Alain NOEL - 2023</p>
+              <p>Les icônes Wolwerine, Iron-man, Spider-man, Batman sont prises sur iconFinder.com</p>
+          </div>
+      </footer>
+  )
+}
+
+export default Footer
