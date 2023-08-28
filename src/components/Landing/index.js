e920619d806e72b8a33f543fac7ac7965a557d55
Alain Noel <alain.noel34@gmail.com>
2023-08-28 15:29:37 +0200

    landing page add button

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useRef, useEffect, useState, Fragment } from "react";
 
 const Landing = () => {
-  return (
-      <main className="welcomePage">
 
+  const [btn, setBtn] = useState(null);
+
+  const refWolwerine = useRef(false);
+
+  useEffect(() => {
+    refWolwerine.current.classList.add("startingImg");
+    setTimeout(() => {
+        refWolwerine.current.classList.remove("startingImg");
+        setBtn(true)
+      }, 1000);
+
+  }, [])
+
+  const displayBtn = btn && (
+    <Fragment>
+      <div className="leftBox">
+          <button className="btn-welcome">Inscription</button>
+      </div>
+      <div className="rightBox">
+          <button className="btn-welcome">Connexion</button>
+      </div>
+    </Fragment>
+  )
+
+  return (
+      <main ref={refWolwerine} className="welcomePage">
+          { displayBtn }
       </main>
   )
 }
