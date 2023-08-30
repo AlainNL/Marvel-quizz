62b689de09ab5ce346b3b1e9b554060f25a58700
Alain Noel <alain.noel34@gmail.com>
2023-08-30 20:41:02 +0200

    config login firebase V9

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../Firebase/firebaseConfig';
 
-const Login= () => {
+const Login= (props) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [btn, setBtn] = useState(false);
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+      if (password.length > 5 && email !== '') {
+          setBtn(true)
+      } else if (btn) {
+          setBtn(false)
+      }
+  }, [password, email, btn])
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then(user => {
+        setEmail('');
+        setPassword('');
+        navigate('/welcome', { replace: true});
+    })
+    .catch(error => {
+        setError(error);
+        setEmail('');
+        setPassword('');
+    })
+
+  }
+
   return (
     <div className="signUpLoginBox">
         <div className="slContainer">
@@ -8,29 +43,26 @@ const Login= () => {
             </div>
             <div className="formBoxRight">
                 <div className="formContent">
-                <h2>Inscription</h2>
+
+                    {error !== '' && <span>{error.message}</span>}
+
+                    <h2>Connexion</h2>
                     <form onSubmit={handleSubmit}>
+
                         <div className="inputBox">
-                            <input onChange={handleChange} value={pseudo} type="text" id="pseudo" autoComplete="off" required />
-                            <label htmlFor="pseudo">Pseudo</label>
-                        </div>
-                        <div className="inputBox">
-                            <input onChange={handleChange} value={email} type="email" id="email" autoComplete="off" required />
+                            <input onChange={e => setEmail(e.target.value)} value={email} type="email" autoComplete="off" required />
                             <label htmlFor="email">Email</label>
                         </div>
                         <div className="inputBox">
-                            <input onChange={handleChange} value={password} type="password" id="password" autoComplete="off" required />
+                            <input onChange={e => setPassword(e.target.value)} value={password} type="password" autoComplete="off" required />
                             <label htmlFor="password">Mot de passe</label>
                         </div>
-                        <div className="inputBox">
-                            <input onChange={handleChange} value={confirmPassword} type="password" id="confirmPassword" autoComplete="off" required />
-                            <label htmlFor="confirmPassword">Confirmer mot de passe</label>
-                        </div>
 
-                        {btn}
+                        {<button disabled={btn ? false : true}>Connexion</button>}
+
                     </form>
                     <div className="linkContainer">
-                        <Link className="simpleLink" to="/login">Déjà inscrit ? Connectez-vous.</Link>
+                        <Link className="simpleLink" to="/signup">Nouveau sur Marvel Quizz ? Inscrivez-vous maintenant.</Link>
                     </div>
                 </div>
             </div>
