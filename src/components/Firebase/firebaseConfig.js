11332710be3036b43282b7ce77395e428f6e69bd
Alain Noel <alain.noel34@gmail.com>
2023-08-30 17:08:55 +0200

    login page in progress

--- src/components/Firebase/firebaseConfig.js
+++ src/components/Firebase/firebaseConfig.js
@@ -10,7 +10,8 @@ const config = {
     storageBucket: "marvel-quizz-bb663.appspot.com",
     messagingSenderId: "59440714534",
     appId: "1:59440714534:web:71c5bd38c839b8aa7ce7e7"
-};
 
+  };
+  
 const app = initializeApp(config);
 export const auth = getAuth(app);
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,7 +4,36 @@ const Login= () => {
   return (
     <div className="signUpLoginBox">
         <div className="slContainer">
-          Login
+        <div className="formBoxLeftLogin">
+            </div>
+            <div className="formBoxRight">
+                <div className="formContent">
+                <h2>Inscription</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="inputBox">
+                            <input onChange={handleChange} value={pseudo} type="text" id="pseudo" autoComplete="off" required />
+                            <label htmlFor="pseudo">Pseudo</label>
+                        </div>
+                        <div className="inputBox">
+                            <input onChange={handleChange} value={email} type="email" id="email" autoComplete="off" required />
+                            <label htmlFor="email">Email</label>
+                        </div>
+                        <div className="inputBox">
+                            <input onChange={handleChange} value={password} type="password" id="password" autoComplete="off" required />
+                            <label htmlFor="password">Mot de passe</label>
+                        </div>
+                        <div className="inputBox">
+                            <input onChange={handleChange} value={confirmPassword} type="password" id="confirmPassword" autoComplete="off" required />
+                            <label htmlFor="confirmPassword">Confirmer mot de passe</label>
+                        </div>
+
+                        {btn}
+                    </form>
+                    <div className="linkContainer">
+                        <Link className="simpleLink" to="/login">Déjà inscrit ? Connectez-vous.</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
