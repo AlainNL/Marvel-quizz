a3d7f2f194c09ab732165d0bd69b2254f558e192
Alain Noel <alain.noel34@gmail.com>
2023-08-31 11:31:07 +0200

    log out config firebase V9

---
+++ src/components/Logout/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { signOut } from "firebase/auth";
+import { auth } from '../Firebase/firebaseConfig';
+import { useNavigate } from 'react-router-dom';
+
+const Logout = () => {
+
+const navigate = useNavigate();
+const [checked, setChecked] = useState(false);
+
+useEffect(() => {
+  if (checked) {
+    signOut(auth).then(() => {
+        console.log("Vous êtes déconnecté");
+        setTimeout(()=> {
+          navigate('/')
+        }, 1000);
+    }).catch((error) => {
+        console.log("Oups, nous avons une erreur !")
+    });
+
+  }
+
+}, [checked, navigate]);
+
+const handleChange = event => {
+    setChecked(event.target.checked)
+}
+
+  return (
+    <div className="logoutContainer">
+        <label className="switch">
+            <input
+                onChange={handleChange}
+                type="checkbox"
+                checked={checked}
+            />
+            <span className="slider round"></span>
+        </label>
+    </div>
+  )
+}
+
+export default Logout
