4da89afd6bb17bb9172685bd5ec87b122a5eac6f
Alain Noel <alain.noel34@gmail.com>
2023-09-01 12:30:54 +0200

    on authstatechanged fix

--- src/components/Firebase/firebaseConfig.js
+++ src/components/Firebase/firebaseConfig.js
@@ -10,8 +10,7 @@ const config = {
     storageBucket: "marvel-quizz-bb663.appspot.com",
     messagingSenderId: "59440714534",
     appId: "1:59440714534:web:71c5bd38c839b8aa7ce7e7"
-
   };
-  
+
 const app = initializeApp(config);
 export const auth = getAuth(app);
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../Firebase/firebaseConfig';
 import Logout from '../Logout'
 import Quiz from '../Quiz'
 
-const Welcome = () => {
-  return (
-    <div className="quiz-bg">
-        <div className="container">
+const Welcome = props => {
+
+  const navigate = useNavigate();
+  const [userSession, setUserSession] = useState(null);
+
+
+  useEffect(() => {
+      const listener = onAuthStateChanged(auth, user => {
+        console.log("hello")
+          user ? setUserSession(user) : navigate('/')
+      })
+
+
+      return listener();
+  }, [])
+
+  return userSession === null ? (
+      <Fragment>
+          <div className="loader">
+          </div>
+      </Fragment>
+  ) : (
+      <div className="quiz-bg">
+          <div className="container">
             <Logout />
             <Quiz />
-        </div>
-    </div>
+          </div>
+      </div>
   )
 }
 
